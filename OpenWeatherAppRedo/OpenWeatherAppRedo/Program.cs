b89c610dbc48e85cfea5aab64c43523ea0144dca
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-03-07 12:40:51 -0500

    second commit

--- OpenWeatherAppRedo/OpenWeatherAppRedo/Program.cs
+++ OpenWeatherAppRedo/OpenWeatherAppRedo/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+
+namespace OpenWeatherAppREDO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var client = new HttpClient();
+
+            Console.WriteLine("Please enter your API Key:");
+            var key = Console.ReadLine();
+
+            Console.WriteLine("What city would you like to check the weather?");
+            var city = Console.ReadLine();
+
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={key}&units=imperial";
+
+            var response = client.GetStringAsync(weatherURL).Result;
+
+
+
+            
+        }
+
+
+
+    }
+
+
+
+
+
+}
+
